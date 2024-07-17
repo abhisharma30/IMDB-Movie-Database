af6201b324bdc4c5a6c31e529a0cb342c900d823
Abhishek Sharma <AbhishekSharma300194@bitbucket.org>
2024-07-17 08:51:30 +0530

    Initial Commit

---
+++ IMDB-Movie-Database/IMDB_Movie_DatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMDB_Movie_DatabaseApp.swift
+//  IMDB-Movie-Database
+//
+//  Created by Abhishek Sharma on 17/07/24.
+//
+
+import SwiftUI
+
+@main
+struct IMDB_Movie_DatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
